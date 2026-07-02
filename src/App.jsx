4b02ca029f5de2eeb563623efba244f38a0cce8b
Dionicio <floresdionicio681@gmail.com>
2026-07-01 19:19:25 -0600

    Completed React-Tv implemetnting componets

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
+import "./App.css";
+import { useState } from "react";
+import ShowSelection from "./shows/ShowSelection";
+import ShowDetails from "./shows/ShowDetails";
+import { tvShows } from "./shows/data";
+
+
 /**
  * React TV is an web streaming platform that allows users to browse
  * through the episodes of a variety of different shows.
  */
+
 export default function App() {
+  const[shows] = useState(tvShows);
+  const[selectedShow, setSelectedShow] = useState(null);
   return (
     <>
       <header>
-        <p>React TV</p>
+        <p>React TV Series</p>
+      <ShowSelection shows={shows} setSelectedShow={ setSelectedShow}/>
+      
       </header>
-      <main></main>
+      <main>
+      <ShowDetails show={selectedShow}/>
+      </main>
     </>
   );
 }
--- src/shows/ShowDetails.jsx
+++ src/shows/ShowDetails.jsx
@@ -1,6 +1,26 @@
+import EpisodeList from "../episodes/EpisodeList"
+import EpisodeDetails from "../episodes/EpisodeDetails";
 import "./shows.css";
+import { useState } from "react";
+
 
 /** Allows users to browse through the episodes of the given show */
-export default function ShowDetails() {
-  return <div className="show-details"></div>;
+export default function ShowDetails({show}) {
+  const [selectedEpisode, setSelectedEpisode] = useState(null);
+   
+  if(!show){
+    return <section>
+      <p>Please select a show to view its episodes</p>
+    </section>
+  }
+  return (
+  <div className="show-details">
+    <EpisodeList name={show.name}
+      episodes={show.episodes}
+      selectedEpisode={selectedEpisode}
+      setSelectedEpisode={setSelectedEpisode}
+      />
+    <EpisodeDetails episode={selectedEpisode}/> 
+  </div>
+  )
 }
